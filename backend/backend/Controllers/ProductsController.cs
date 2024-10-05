5534c7dc0ffa63f9ebd0f55c6c40e1d983b30893
deb240485 <deb240485rana@gmail.com>
2024-10-05 19:52:57 +0530

    petsotre complete in react

---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using backend.Context;
+using backend.Dtos;
+using backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public ProductsController(ApplicationDBContext dBContext) {
+            _dbContext = dBContext;
+        }
+
+        //Create
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateUpdateProductDto productDto)
+        {
+            var newProduct = new ProductEntity
+            {
+                Brand = productDto.Brand,
+                Title = productDto.Title,
+            };
+            await _dbContext.Products.AddAsync(newProduct);
+            await _dbContext.SaveChangesAsync();
+            return Ok("Product Saved Successfully");
+        }
+
+        //Read
+        [HttpGet]
+        public async Task<ActionResult<List<ProductEntity>>> GetAllProducts()
+        {
+            var products = await _dbContext.Products.OrderByDescending(x=>x.UpdatedAt).ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<ProductEntity>> GetProductById([FromRoute] long id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(product is null)
+            {
+                return NotFound("Product not Found");
+            }
+            return Ok(product);
+        }
+
+        //Update
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] long id, [FromBody] CreateUpdateProductDto updateProductDto)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(product is null)
+            {
+                return NotFound("Product not Found.");
+            }
+
+            product.Brand = updateProductDto.Brand;
+            product.Title = updateProductDto.Title;
+            product.UpdatedAt = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Product updated Successfully.");
+        }
+
+        //Delete
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] long id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product is null)
+            {
+                return NotFound("Product not Found.");
+            }
+
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Product Deleted Successfully.");
+        }
+    }
+}
---
+++ backend/backend/Program.cs
@@ -0,0 +1,40 @@
+using backend.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Config DB.
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(options => {
+    options.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+app.MapControllers();
+
+app.Run();
